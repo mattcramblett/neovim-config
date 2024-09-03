c0a3220e16451787cecb88dbb632278b25753f28
Matt Cramblett <Cramblett5@gmail.com>
2024-09-04 08:20:34 +0900

    initial commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,7 @@
+return {
+  "navarasu/onedark.nvim",
+  name = "onedark",
+  lazy = false,
+  style = "deep",
+  priority = 1000
+}
