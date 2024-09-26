2177724723e7cefa12cfe275454a47ee23e51adf
Matt Cramblett <Cramblett5@gmail.com>
2024-09-26 18:50:08 -0400

    add todo-comments plugin

---
+++ lua/plugins/visuals.lua
@@ -0,0 +1,9 @@
+return {
+	-- Highlight TODO, NOTE, FIXME
+  {
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -74,4 +74,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
