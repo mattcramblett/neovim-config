da623634ddcfad189b5704ebc780a1ef3efee0a1
Matt Cramblett <matthew.cramblett@upstart.com>
2024-10-03 14:01:19 -0400

    update neotest options

--- lua/plugins/neo-test.lua
+++ lua/plugins/neo-test.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-neotest/neotest",
+	lazy = true,
 	dependencies = {
 		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
@@ -17,7 +18,31 @@ return {
 		local neotest = require("neotest")
 		neotest.setup({
 			adapters = {
-				require("neotest-rspec"), -- https://github.com/olimorris/neotest-rspec?tab=readme-ov-file#wrench-configuration
+				require("neotest-rspec")({
+					filter_dirs = { ".git", "node_modules", "sorbet", "client", "client_monorepo", "db", "script", "log" },
+					-- Optionally your function can take a position_type which is one of:
+					-- - "file"
+					-- - "test"
+					-- - "dir"
+					rspec_cmd = function(position_type)
+						-- ensure generated tests (ex: .each loops with an example) will fail past the first iteration if needed.
+						-- only do this when running an individual test because otherwise the whole file fails if one example fails.
+						if position_type == "test" then
+							return vim.tbl_flatten({
+								"bundle",
+								"exec",
+								"rspec",
+								"--fail-fast",
+							})
+						else
+							return vim.tbl_flatten({
+								"bundle",
+								"exec",
+								"rspec",
+							})
+						end
+					end,
+				}),
 				require("neotest-jest")({ -- https://github.com/nvim-neotest/neotest-jest?tab=readme-ov-file#monorepos
 					jestCommand = "yarn test --",
 					jestConfigFile = "jest.config.ts",
@@ -30,36 +55,48 @@ return {
 			},
 		})
 	end,
-  keys = {
-    {
-      "<leader>t",
-      function() require("neotest").run.run() end,
-      desc = "Run nearest [t]est"
-    },
-    {
-      "<leader>T",
-      function () require("neotest").run.run(vim.fn.expand("%")) end,
-      desc = "Run all [T]ests in current file"
-    },
-    {
-		  "<leader>td",
-      function() require("neotest").run.run({ strategy = "dap" }) end,
-      desc = "[T]est - [D]ebug nearest"
-    },
-    {
-      "<leader>ts",
-      function() require("neotest").run.stop() end,
-      desc = "[T]est [S]top nearest"
-    },
-    {
-      "<leader>ta",
-      function () require("neotest").run.attach() end,
-      desc = "[T]est - [A]ttach to nearest"
-    },
-    {
-      "<leader>to",
-      function() require("neotest").output.open({ enter = true, auto_close = true }) end,
-      desc = "Show [T]est [O]utput"
-    }
-  },
+	keys = {
+		{
+			"<leader>t",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Run nearest [t]est",
+		},
+		{
+			"<leader>T",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			desc = "Run all [T]ests in current file",
+		},
+		{
+			"<leader>td",
+			function()
+				require("neotest").run.run({ strategy = "dap" })
+			end,
+			desc = "[T]est - [D]ebug nearest",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").run.stop()
+			end,
+			desc = "[T]est [S]top nearest",
+		},
+		{
+			"<leader>ta",
+			function()
+				require("neotest").run.attach()
+			end,
+			desc = "[T]est - [A]ttach to nearest",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output.open({ enter = true, auto_close = true })
+			end,
+			desc = "Show [T]est [O]utput",
+		},
+	},
 }
