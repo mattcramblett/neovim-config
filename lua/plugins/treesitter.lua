dcd88c522b5cfc22d3fbed3b6d372d3e0ee837cd
Matt Cramblett <Cramblett5@gmail.com>
2024-10-06 10:55:08 -0400

    add treesitter endwise

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "ruby", "typescript", "kotlin", "java", "rust" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "lua", "javascript", "ruby", "typescript", "kotlin", "java", "rust" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"RRethy/nvim-treesitter-endwise",
+		event = "InsertEnter",
+	},
 }
