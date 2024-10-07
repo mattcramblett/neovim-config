54f5c4e936ef01d7d90e04c79af0181227d7eadb
Matt Cramblett <matthew.cramblett@upstart.com>
2024-10-07 11:08:16 -0400

    restructure formatter config

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,43 +1,41 @@
 -- Formatter
 return {
-  "stevearc/conform.nvim",
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        require("conform").format({ async = true })
-      end,
-      mode = "n",
-      desc = "[F]ormat [F]ile",
-    },
-  },
-  -- This will provide type hinting with LuaLS
-  ---@module "conform"
-  ---@type conform.setupOpts
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      ruby = { "rubocop" },
-      kotlin = { "ktlint" },
-      java = { "google-java-format" },
-      rust = { "rustfmt", lsp_format = "fallback" },
-      python = { "isort", "black" },
-      typescript = {'prettier'},
-      typescriptreact = {'prettier'},
-      javascript = {'prettier'},
-      javascriptreact = {'prettier'},
-      json = {'prettier'},
-      html = {'prettier'},
-      css = {'prettier'},
-      scss = {'prettier'},
-      markdown = {'prettier'},
-      yaml = {'prettier'},
-    },
-    -- Set default options
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-  },
+	"stevearc/conform.nvim",
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "n",
+			desc = "[F]ormat [F]ile",
+		},
+	},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				ruby = { "rubocop" },
+				kotlin = { "ktlint" },
+				java = { "google-java-format" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				python = { "isort", "black" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				json = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				scss = { "prettier" },
+				markdown = { "prettier" },
+				yaml = { "prettier" },
+        ["_"] = { "trim_whitespace" }, -- filetype with no formatter
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+		})
+	end,
 }
