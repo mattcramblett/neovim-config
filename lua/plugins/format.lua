17d8362749bed8e67f3199f573e1c2b55a464d09
Matt Cramblett <matthew.cramblett@upstart.com>
2024-10-07 14:45:16 -0400

    cleanup formatters until needed

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -20,17 +20,10 @@ return {
 				kotlin = { "ktlint" },
 				java = { "google-java-format" },
 				rust = { "rustfmt", lsp_format = "fallback" },
-				python = { "isort", "black" },
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				javascript = { "prettier" },
 				javascriptreact = { "prettier" },
-				json = { "prettier" },
-				html = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				markdown = { "prettier" },
-				yaml = { "prettier" },
 				["_"] = { "trim_whitespace" }, -- filetype with no formatter
 			},
 			default_format_opts = {
