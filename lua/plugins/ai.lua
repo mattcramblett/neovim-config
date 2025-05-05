eedd84ad59073b1a466a07934121671852c6f2d9
Matt Cramblett <Cramblett5@gmail.com>
2025-05-04 21:55:51 -0400

    add code companion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,19 +1,68 @@
+if not vim.env.OPENAI_API_KEY then
+	return {}
+end
+
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    config = function()
-      -- run :Copilot auth
-      require("copilot").setup({})
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    config = function ()
-      require("copilot_cmp").setup()
-    end
-  }
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("codecompanion").setup({
+				adapters = {
+					openai = function()
+						return require("codecompanion.adapters").extend("openai", {
+							schema = {
+								model = {
+									default = "o4-mini",
+								},
+							},
+						})
+					end,
+				},
+				strategies = {
+					chat = {
+						adapter = "openai",
+					},
+					inline = {
+						adapter = "openai",
+					},
+					cmd = {
+						adapter = "openai",
+					},
+				},
+			})
+			vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<LocalLeader>a",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+			-- Expand 'cc' into 'CodeCompanion' in the command line
+			vim.cmd([[cab cc CodeCompanion]])
+		end,
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		suggestion = { enabled = false },
+		panel = { enabled = false },
+		config = function()
+			-- run :Copilot auth
+			require("copilot").setup({})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
