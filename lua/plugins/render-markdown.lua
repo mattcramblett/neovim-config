86e9eaa4e3509b46effb38983846236f9b4360ff
Matt Cramblett <Cramblett5@gmail.com>
2025-05-08 20:30:42 -0400

    codecompanion markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	opts = {
-		file_types = { "markdown", "Avante" },
+		file_types = { "markdown", "Avante", "codecompanion" },
 	},
 	ft = { "markdown", "Avante" },
 }
