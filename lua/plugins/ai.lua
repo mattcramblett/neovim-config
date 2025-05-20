53edd8514a63890e724e15380840d230773af2a3
Matt Cramblett <matthew.cramblett@upstart.com>
2025-05-19 20:03:52 -0400

    fix copilot node path

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -58,7 +58,7 @@ return {
 		config = function()
 			-- run :Copilot auth
 			require("copilot").setup({
-				copilot_node_command = "~/.asdf/installs/nodejs/22.12.0/bin/node",
+				copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/22.12.0/bin/node",
 				keymap = {
 					jump_prev = "[[",
 					jump_next = "]]",
