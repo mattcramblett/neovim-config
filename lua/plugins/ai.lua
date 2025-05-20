0280e2f5cca61426243a8499b617217fef698a47
Matt Cramblett <matthew.cramblett@upstart.com>
2025-05-20 09:37:27 -0400

    remove default keymaps from goose

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -4,26 +4,26 @@ return {
 		"azorng/goose.nvim",
 		config = function()
 			require("goose").setup({
-				default_global_keymaps = true,
-				keymaps = {
+				default_global_keymaps = false,
+				keymap = {
 					global = {
-						toggle = "<leader>gg", -- Open goose. Close if opened
-						open_input = "<leader>gi", -- Opens and focuses on input window on insert mode
-						open_input_new_session = "<leader>gI", -- Opens and focuses on input window on insert mode. Creates a new session
-						open_output = "<leader>go", -- Opens and focuses on output window
-						toggle_focus = "<leader>gt", -- Toggle focus between goose and last window
-						close = "<leader>gq", -- Close UI windows
+						toggle = "<C-g>", -- Open goose. Close if opened
+						-- open_input = "<leader>gi", -- Opens and focuses on input window on insert mode
+						-- open_input_new_session = "<leader>gI", -- Opens and focuses on input window on insert mode. Creates a new session
+						-- open_output = "<leader>go", -- Opens and focuses on output window
+						-- toggle_focus = "<leader>gt", -- Toggle focus between goose and last window
+						-- close = "<leader>gq", -- Close UI windows
 						toggle_fullscreen = "<leader>gf", -- Toggle between normal and fullscreen mode
-						select_session = "<leader>gs", -- Select and load a goose session
-						goose_mode_chat = "<leader>gmc", -- Set goose mode to `chat`. (Tool calling disabled. No editor context besides selections)
-						goose_mode_auto = "<leader>gma", -- Set goose mode to `auto`. (Default mode with full agent capabilities)
+						-- select_session = "<leader>gs", -- Select and load a goose session
+						-- goose_mode_chat = "<leader>gmc", -- Set goose mode to `chat`. (Tool calling disabled. No editor context besides selections)
+						-- goose_mode_auto = "<leader>gma", -- Set goose mode to `auto`. (Default mode with full agent capabilities)
 						-- configure_provider = "<leader>gp", -- Quick provider and model switch from predefined list
-						diff_open = "<leader>gd", -- Opens a diff tab of a modified file since the last goose prompt
-						diff_next = "<leader>g]", -- Navigate to next file diff
-						diff_prev = "<leader>g[", -- Navigate to previous file diff
+						-- diff_open = "<leader>gd", -- Opens a diff tab of a modified file since the last goose prompt
+						-- diff_next = "<leader>g]", -- Navigate to next file diff
+						-- diff_prev = "<leader>g[", -- Navigate to previous file diff
 						-- diff_close = "<leader>gc", -- Close diff view tab and return to normal editing
-						diff_revert_all = "<leader>gra", -- Revert all file changes since the last goose prompt
-						diff_revert_this = "<leader>grt", -- Revert current file changes since the last goose prompt
+						-- diff_revert_all = "<leader>gra", -- Revert all file changes since the last goose prompt
+						-- diff_revert_this = "<leader>grt", -- Revert current file changes since the last goose prompt
 					},
 					window = {
 						submit = "<cr>", -- Submit prompt
@@ -37,6 +37,9 @@ return {
 						next_prompt_history = "<down>", -- Navigate to next prompt in history
 					},
 				},
+        providers = {
+          openai = "4o-mini"
+        }
 			})
 		end,
 		dependencies = {
