4cb62a4f93f042592cc1fb5c2edc5d5ac40acfe0
Matt Cramblett <matthew.cramblett@upstart.com>
2025-06-09 09:32:05 -0400

    don't autotrigger completions

--- lua/lsp-init.lua
+++ lua/lsp-init.lua
@@ -44,7 +44,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 
     if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
     end
   end,
 })
