b1ae8e902202b29eda43409bc8b62bf778559daa
Matt Cramblett <Cramblett5@gmail.com>
2025-06-15 22:58:18 -0400

    tundra colorscheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,22 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
-    local theme = require("lualine.themes.onedark")
-    theme.normal.b.bg = '#05183d'
-    theme.normal.c.bg = '#020a1a'
+    -- local theme = require("lualine.themes.onedark")
+    -- theme.normal.b.bg = '#05183d'
+    -- theme.normal.c.bg = '#020a1a'
 
 		require("lualine").setup({
 			options = {
-				theme = theme,
+				theme = 'tundra',
 			},
 			sections = {
 				lualine_a = {
 					{
 						"filename",
-						file_status = true,
-						newfile_status = true,
-						path = 3,
-						shorting_target = 42,
+						-- file_status = true,
+						-- newfile_status = true,
+						-- path = 3,
+						-- shorting_target = 42,
 					},
 				},
 			},
