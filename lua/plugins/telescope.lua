caa30fbff569b4e3f5085b91f415838a30b9684b
Matt Cramblett <Cramblett5@gmail.com>
2025-10-19 19:49:00 -0400

    retire branch changes for now

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -115,35 +115,4 @@ return {
 			end, { desc = "[G]o to [O]pen file (search files with word)" })
 		end,
 	},
-  --
-  -- CUSTOM PICKERS
-  --
-	{
-		"axkirillov/easypick.nvim",
-		config = function()
-			-- Setup pickers with arbitray commands
-			local get_default_branch = "git remote show origin | grep 'HEAD branch' | cut -d' ' -f5"
-			local base_branch = vim.fn.system(get_default_branch) or "main"
-			local easypick = require("easypick")
-			local previewers = require("telescope.previewers")
-			easypick.setup({
-				pickers = {
-					-- diff current branch with base_branch and show files that changed with respective diffs in preview
-					{
-						name = "branch_changes",
-						command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " ) && git ls-files --others --exclude-standard",
-						previewer = previewers.new_termopen_previewer({
-							get_command = function(entry)
-								-- entry.path will be the full path when using built-in file/git pickers
-								local filepath = entry.path or entry.value
-								return "git diff " .. base_branch .. " -- " .. filepath
-							end,
-						}),
-					},
-				},
-			})
-
-			vim.keymap.set("n", ".", "<cmd>:Easypick branch_changes<CR>", { desc = "Changed files on branch" })
-		end,
-	},
 }
