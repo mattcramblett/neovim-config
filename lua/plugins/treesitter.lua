4b19f14bba6854182c5258f0c113d53f01c680c6
Matt <matthew.cramblett@upstart.com>
2026-05-01 16:04:04 -0400

    update treesitter config, remove lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "lua", "javascript", "ruby", "typescript", "kotlin", "java", "rust", "zig" },
+			ensure_installed = { "javascript", "ruby", "typescript", "kotlin", "java", "rust", "zig" },
 			auto_install = true,
 		},
 	},
