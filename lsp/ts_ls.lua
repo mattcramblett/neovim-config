e31f8b0172644771bcd8ef4e31dac06048112a86
Matt <matthew.cramblett@upstart.com>
2026-05-06 10:59:15 -0400

    point typescript-language-server to nearest tsconfig.json

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,5 +1,5 @@
 return {
   cmd = { "typescript-language-server", "--stdio" },
-  root_markers = { ".git", "package.json" },
+  root_markers = { "tsconfig.json", ".git", "package.json" },
   filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
 }
